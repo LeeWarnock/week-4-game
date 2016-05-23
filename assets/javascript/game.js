4b26a11f4ed90ccbfb5a12b4710d898c858e1142
Lee Warnock <clwarnock@gmail.com>
2016-05-23 07:29:22 -0500

    adding sounds

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+//VARS - Using a lot of Holly Sprinstein's code
+var music = {},
+    types = [],
+    gameData = {}
+    attackName = '',
+    curAttack = {},
+    randInt = 0,
+    enemyAttack = {},
+    characters = [],
+    defendProgressInt = null,
+    defendProgressComplete = 0,
+    progressInt = null,
+    progressComplete = 0;
